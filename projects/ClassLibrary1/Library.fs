aa93af0403772c035221aa31d1b8a6a839c76b86
Leo <leononame@lho.io>
2024-01-14 14:12:03 +0100

    initial commit

---
+++ projects/ClassLibrary1/Library.fs
@@ -0,0 +1,12 @@
+﻿module Repro
+open Fable.Core
+
+[<Import("log", "../../../../features/accounting/amount/Amount")>]
+let log: string -> unit = jsNative
+
+[<Import("log", "pkg/log")>]
+let log2: string -> unit = jsNative
+
+let sayHello =
+    log "Hello"
+    log2 "World"
